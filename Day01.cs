d7a340a3a67924405c472f84278b55b424f45c9a
Meowth <NoThisIsAPublicRepo@No.No>
2023-12-01 08:52:57 +0100

    oh well

--- Day01.cs
+++ Day01.cs
@@ -9,27 +9,54 @@ namespace Advent2023
 {
     public class Day01 : Day
     {
-        List<int> Instructions;
+        string[] Instructions;
         public Day01(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            Instructions = this.ParseListOfInteger(Input);
+            Instructions = this.ParseStringArray(Input);
         }
         public override Tuple<string, string> GetResult()
         {
-            return Tuple.Create(GetPartOne(), GetPartTwo());
+            return Tuple.Create(GetPartOne(Instructions), GetPartTwo());
         }
-        public string GetPartOne()
+        public string GetPartOne(string[] instructions)
         {
             int ReturnValue = 0;
-
+            foreach(string s in instructions)
+            {
+                List<int> ints = new List<int>();
+                foreach (char c in s)
+                    if (c >= '0' && c <= '9')
+                        ints.Add(c-'0');
+                ReturnValue += ints.First() * 10 + ints.Last();
+            }
             return ReturnValue.ToString();
         }
         public string GetPartTwo()
         {
-            int ReturnValue = 0;
-
-            return ReturnValue.ToString();
+            List<string> Strints = new List<string>
+            {
+                "one",
+                "two",
+                "three",
+                "four",
+                "five",
+                "six",
+                "seven",
+                "eight",
+                "nine"
+            };
+            List<string> instructionList = new List<string>();
+            foreach(string s in Instructions)
+            {
+                string sch = s;
+                for (int i = 1; i <= 9; i++)
+                {
+                    sch = sch.Replace(Strints[i - 1], Strints[i - 1] + i.ToString() + Strints[i - 1]);
+                }
+                instructionList.Add(sch);
+            }
+            return GetPartOne(instructionList.ToArray());
         }
     }
 }
