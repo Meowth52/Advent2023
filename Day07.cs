1114b51f7250c46542e35116f099c2ab2cd4cdf2
Meowth52 <meowth@meowth.se>
2023-12-07 22:26:39 +0100

    phew

--- Day07.cs
+++ Day07.cs
@@ -16,7 +16,7 @@ namespace Advent2023
         public Day07(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            Input = Input.Replace('T','a');
+            Input = Input.Replace('T', 'a');
             Input = Input.Replace('J', 'b');
             Input = Input.Replace('Q', 'c');
             Input = Input.Replace('K', 'd');
@@ -29,7 +29,7 @@ namespace Advent2023
                 List<char> sortHand = instruction[0].ToCharArray().ToList();
                 sortHand.Sort();
                 Hands.Add(instruction[0], (1, Int32.Parse(instruction[1])));
-                JokerHands.Add(instruction[0].Replace('b','1'), (1, Int32.Parse(instruction[1])));
+                JokerHands.Add(instruction[0].Replace('b', '1'), (1, Int32.Parse(instruction[1])));
             }
             Values = new Dictionary<char, int>
             {
@@ -83,11 +83,11 @@ namespace Advent2023
                 Hands[hand.Key] = (Winnability, hand.Value.Bet);
             }
             int number = 1;
-            List<string> debug= new List<string>();
-            for(int i = 1; i <= 7; i++)
+            List<string> debug = new List<string>();
+            for (int i = 1; i <= 7; i++)
             {
-                var sortedGroup = Hands.Where(x => x.Value.Winnability == i).OrderBy(x=>x.Key);
-                foreach(var item in sortedGroup)
+                var sortedGroup = Hands.Where(x => x.Value.Winnability == i).OrderBy(x => x.Key);
+                foreach (var item in sortedGroup)
                 {
                     ReturnValue += number * item.Value.Bet;
                     number++;
@@ -101,7 +101,7 @@ namespace Advent2023
         {
             int ReturnValue = 0;
             var MenJagVillÄndra = JokerHands;
-                foreach (KeyValuePair<string, (int Winnability, int Bet)> hand in MenJagVillÄndra)
+            foreach (KeyValuePair<string, (int Winnability, int Bet)> hand in MenJagVillÄndra)
             {
                 int Winnability = 0;
                 IEnumerable<IGrouping<char, char>> groups = hand.Key.ToCharArray().GroupBy(x => x);
@@ -112,14 +112,14 @@ namespace Advent2023
                 }
                 numbers.Sort((a, b) => b.CompareTo(a));
                 int jokers = 0;
-                if(groups.FirstOrDefault(g => g.Key == '1')!=null)
+                if (groups.FirstOrDefault(g => g.Key == '1') != null)
                     jokers = groups.First(g => g.Key == '1').Count();
 
                 if (jokers == 1)
                     numbers[0] += jokers;
                 else if (jokers == 2)
                 {
-                    numbers[0] = numbers[1] + jokers;
+                    numbers[0] += numbers[1];
                     numbers[1] = 1;
                 }
                 else if (jokers >= 3 && jokers != 5)
@@ -145,6 +145,8 @@ namespace Advent2023
                     if (numbers[0] == 1)
                         Winnability = 1; // -.-
                 }
+                if (Winnability > 7)
+                    ;
                 JokerHands[hand.Key] = (Winnability, hand.Value.Bet);
             }
             int number = 1;
@@ -164,7 +166,3 @@ namespace Advent2023
         }
     }
 }
-//245919897     high
-
-//245807166 nope
-//245711189  low
