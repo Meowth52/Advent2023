25b2fd8489f78014f0232d97a6ce655e16aa8d14
Meowth52 <meowth@meowth.se>
2023-12-12 17:14:58 +0100

    9

--- Day09.cs
+++ Day09.cs
@@ -9,11 +9,11 @@ namespace Advent2023
 {
     public class Day09 : Day
     {
-        List<int> Instructions;
+        List<List<int>> Instructions;
         public Day09(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            Instructions = this.ParseListOfInteger(Input);
+            Instructions = this.ParseListOfIntegerLists(Input);
         }
         public override Tuple<string, string> GetResult()
         {
@@ -22,14 +22,81 @@ namespace Advent2023
         public string GetPartOne()
         {
             int ReturnValue = 0;
+            foreach (List<int> Instruction in Instructions)
+            {
+                List<int> Next = GetAnotheList(Instruction);
+                List<List<int>> MooooreLists = new List<List<int>>();
+                MooooreLists.Add(Instruction);
+                MooooreLists.Add(Next);
+                bool Done = false;
+                while (!Done)
+                {
+                    Done = true;
+                    Next = GetAnotheList(Next);
+                    MooooreLists.Add(Next);
+                    foreach (int x in Next)
+                    {
+                        if (x != 0)
+                        {
+                            Done = false;
+                            break;
+                        }
+                    }
+                }
+                int NextInt = 0;
+                for (int i = MooooreLists.Count - 1; i >= 0; i--)
+                {
+                    NextInt += MooooreLists[i][MooooreLists[i].Count - 1];
+                    ;
+                }
+                ReturnValue += NextInt;
+            }
 
             return ReturnValue.ToString();
         }
         public string GetPartTwo()
         {
             int ReturnValue = 0;
+            foreach (List<int> Instruction in Instructions)
+            {
+                List<int> Next = GetAnotheList(Instruction);
+                List<List<int>> MooooreLists = new List<List<int>>();
+                MooooreLists.Add(Instruction);
+                MooooreLists.Add(Next);
+                bool Done = false;
+                while (!Done)
+                {
+                    Done = true;
+                    Next = GetAnotheList(Next);
+                    MooooreLists.Add(Next);
+                    foreach (int x in Next)
+                    {
+                        if (x != 0)
+                        {
+                            Done = false;
+                            break;
+                        }
+                    }
+                }
+                int NextInt = 0;
+                for (int i = MooooreLists.Count - 1; i >= 0; i--)
+                {
+                    NextInt = MooooreLists[i][0] - NextInt;
+                    ;
+                }
+                ReturnValue += NextInt;
+            }
 
             return ReturnValue.ToString();
+        }
+        List<int> GetAnotheList(List<int> list)
+        {
+            List<int> ReturnList = new List<int>();
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                ReturnList.Add(list[i + 1] - list[i]);
+            }
+            return ReturnList;
         }
     }
 }
