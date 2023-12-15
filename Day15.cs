a115675aa3c9e0717f9815c60080125baebe281c
Meowth <NoThisIsAPublicRepo@No.No>
2023-12-15 17:11:05 +0100

    Nä nu "går" jag hem

--- Day15.cs
+++ Day15.cs
@@ -9,11 +9,13 @@ namespace Advent2023
 {
     public class Day15 : Day
     {
-        List<int> Instructions;
+        string[] Instructions;
+        Dictionary<int,Box> Boxes;
         public Day15(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            Instructions = this.ParseListOfInteger(Input);
+            Instructions = this.ParseStringArray(Input, ",");
+            Boxes= new Dictionary<int,Box>();
         }
         public override Tuple<string, string> GetResult()
         {
@@ -22,14 +24,77 @@ namespace Advent2023
         public string GetPartOne()
         {
             int ReturnValue = 0;
+            foreach (string s in Instructions)
+            {
+                ReturnValue += Hashis(s);
+            }
 
             return ReturnValue.ToString();
         }
         public string GetPartTwo()
         {
             int ReturnValue = 0;
+            for (int i = 0;i<256;i++)
+            {
+                Boxes.Add(i, new Box(i));
+            }
+            foreach (string s in Instructions)
+            {
+                string hashstring = s.Replace('=','-');
+                string lensId = hashstring.Split('-').First();
+                int id = Hashis(lensId);
+                if (s.Contains('='))
+                {
+                    int lensStrenght = this.ParseListOfInteger(s).First();
+                    if (Boxes[id].Lenses.ContainsKey(lensId))
+                        Boxes[id].Lenses[lensId] = lensStrenght;
+                    else
+                    {
+                        Boxes[id].Lenses.Add(lensId, lensStrenght);
+                    }
+
+                }
+                else
+                {
+
+                    if (Boxes[id].Lenses.ContainsKey(lensId))
+                    {
+                        Boxes[id].Lenses.Remove(lensId);
+                    }
+                }
+            }
+            foreach(Box box in Boxes.Values)
+            {
+                int i = 0;
+                foreach(int lens in box.Lenses.Values)
+                {
+                    i++;
+                    ReturnValue += (box.Id + 1) * (i) * lens;
+                }
+            }
 
             return ReturnValue.ToString();
+        }
+        int Hashis(string hhaassshh)
+        {
+            int CurrentValue = 0;
+            foreach (char c in hhaassshh)
+            {
+                CurrentValue += (int)c;
+                CurrentValue *= 17;
+                CurrentValue %= 256;
+            }
+            return CurrentValue;
+        }
+    }
+    public class Box
+    {
+        public int Id;
+        public Dictionary<string,int> Lenses;
+        public Box(int id)
+        {
+            Id = id;
+            Lenses = new Dictionary<string, int>();
         }
     }
 }
