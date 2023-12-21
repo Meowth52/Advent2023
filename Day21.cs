3063331ec50a76fc88749a526ec43787736e9f18
Meowth52 <meowth@meowth.se>
2023-12-21 17:17:53 +0100

    nope

--- Day21.cs
+++ Day21.cs
@@ -4,16 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.Collections;
 
 namespace Advent2023
 {
     public class Day21 : Day
     {
-        List<int> Instructions;
+        HashSet<Coordinate> Plots;
+        int MaxX;
+        int MaxY;
+        Coordinate Start;
         public Day21(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            Instructions = this.ParseListOfInteger(Input);
+            string[] strings = this.ParseStringArray(Input);
+            Plots = new HashSet<Coordinate>();
+            for (int y = 0; y < strings.Length; y++)
+            {
+                for (int x = 0; x < strings[0].Length; x++)
+                {
+                    if (strings[y][x] != '#')
+                    {
+                        Plots.Add(new Coordinate(x, strings.Length - (y + 1)));
+                        if (strings[y][x] == 'S')
+                            Start = new Coordinate(x, strings.Length - (y + 1));
+                    }
+                }
+            }
+            MaxY = strings.Length - 1;
+            MaxX = strings[0].Length - 1;
         }
         public override Tuple<string, string> GetResult()
         {
@@ -22,13 +41,39 @@ namespace Advent2023
         public string GetPartOne()
         {
             int ReturnValue = 0;
-
+            int NrOfSteps = 64;
+            char[] Directions =
+            {
+                'N','E','S','W'
+            };
+            HashSet<Coordinate> Possibilities = new HashSet<Coordinate>()
+            {
+                Start
+            };
+            for (int i = 0; i < NrOfSteps; i++)
+            {
+                HashSet<Coordinate> Next = new HashSet<Coordinate>();
+                foreach (Coordinate possibility in Possibilities)
+                {
+                    foreach (char direction in Directions)
+                    {
+                        Coordinate maybe = new Coordinate(possibility);
+                        maybe.MoveNSteps(direction);
+                        if (Plots.Contains(maybe))
+                        {
+                            Next.Add(maybe);
+                        }
+                    }
+                }
+                Possibilities = new HashSet<Coordinate>(Next);
+            }
+            ReturnValue = Possibilities.Count;
             return ReturnValue.ToString();
         }
         public string GetPartTwo()
         {
             int ReturnValue = 0;
-
+            //ffs
             return ReturnValue.ToString();
         }
     }
